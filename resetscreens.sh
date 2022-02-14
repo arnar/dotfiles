f6c5bf5d7018c15f66d6a02cdb24fa04d33ee1f3
Arnar Birgisson <arnarb@google.com>
2022-02-14 10:46:27 -0800

    resetscreens (not using so much anymore)

--- resetscreens.sh
+++ resetscreens.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/zsh
 
 # Keymaps
-setxkbmap -option
-setxkbmap -option ctrl:nocaps
-
-# On thinkpad
-builtin_kbd=$(xinput -list | grep 'AT Translated Set 2 keyboard' | sed -rn 's/.*id=([[:digit:]]+).*/\1/p')
-if [[ ! -z $builtin_kbd ]]; then
-    setxkbmap -device $builtin_kbd -option altwin:swap_alt_win
-fi
+setxkbmap -option ctrl:nocaps,altwin:swap_alt_win,compose:ralt
 
 # Cycle compton
-pkill compton
-compton --config $HOME/.dotfiles/compton.conf &
+pkill picom
+picom --config $HOME/.dotfiles/compton.conf &
 
 # Restart polybar
 polybar-msg cmd restart
